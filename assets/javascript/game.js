9bf48ec60b6d8a6356fa0371f36666375081e2cc
murnda <mdelong333@gmail.com>
2019-09-13 00:35:42 -0400

    beginning html, crystal pics

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//Generate random number and write it to .goal-number
+
+
+
+//on click event for buttons - when player clicks a button update score by that quantity - do not show quantity on button
+
+//If players total score matches goal number restart game - increase wins by 1, if players total score exceeds goal number restart game - increase losses
