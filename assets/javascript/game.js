108799ef6a02c00b02874e8fab0e4492728c53de
murnda <mdelong333@gmail.com>
2019-09-13 01:38:00 -0400

    random number generates for crystal value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,27 @@
-//Generate random number between 19-120 and logs it to console
-function randomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    console.log (Math.floor(Math.random() * (max - min)) + min); 
+//Generate random number for goal between 19-120 and logs it to console
+function randomGoalNumber(min, max) {
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
   }
 
 //Assigns random number as goal for player to meet
-var goal = randomNumber(19, 120)
+var goal = randomGoalNumber(19, 120);
+
+
+$(".goal-number").text(goal);
+
+//Generate random number for crystal value between 1-12 and logs it to console
+function randomCrystalValue(min, max) {
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+//Assigns random value to crystal
+var crystalValue = randomCrystalValue(1, 12);
+
 
 //on click event for buttons - logs click to console
 $(".crystal-img").on("click", function() {
+    
+    
     console.log("click!");
 
 
