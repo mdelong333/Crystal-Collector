3147aa20d8a42577442626c43000bd6cf9b8c384
murnda <mdelong333@gmail.com>
2019-09-15 00:27:39 -0400

    player score increases when crystals clicked - html updates - having trouble getting wins and losses to increment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,20 +17,52 @@ $(document).ready(function() {
   
    //on click event for each crystal - generates random number for crystal value and logs click to console
   $("#amethyst").on("click", function() {
+    score = score + amethyst;
+    $("#player-score").text("Total Score: " + score);
     console.log(amethyst);
+    console.log(score);
   });
 
   $("#quartz").on("click", function() {
+    score = score + quartz;
+    $("#player-score").text("Total Score: " + score);
     console.log(quartz);
+    console.log(score);
   })
 
   $("#citrine").on("click", function() {
+    score = score + citrine;
+    $("#player-score").text("Total Score: " + score);
     console.log(citrine);
+    console.log(score);
   })
 
   $("#labradorite").on("click", function() {
+    score = score + labradorite;
+    $("#player-score").text("Total Score: " + score);
     console.log(labradorite);
+    console.log(score);
   })
 
-  console.log(score);
+  if (score === goal) {
+    wins++
+    $("#wins-txt").text("Wins: " + wins);
+  } 
+
+  if (score > goal) {
+    losses++
+    $("#loss-txt").text("Losses: " + losses);
+  }
+
+  
+
+  function reset() {
+    score = 0;
+    goal = Math.floor(Math.random() * (120 - 19 +1)) + 19;
+    $("#goal-number").text("Goal: " + goal);
+    amethyst = Math.floor(Math.random() * 12) + 1;
+    quartz = Math.floor(Math.random() * 12) + 1;
+    citrine = Math.floor(Math.random() * 12) + 1;
+    labradorite = Math.floor(Math.random() * 12) + 1;
+  }
 })
