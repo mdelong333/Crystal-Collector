4f13e4bae5234cd83c3ff71b8615012f31acf86c
murnda <mdelong333@gmail.com>
2019-09-15 00:03:24 -0400

    each crystal gets random value assigned on page load and maintains random value for duration of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,31 @@ $(document).ready(function() {
 
   //Generate random number for goal between 19-120 and logs it to console
    var goal = Math.floor(Math.random() * (120 - 19 +1)) + 19;
-  
    //Adds random goal number to html
    $("#goal-number").text("Goal: " + goal);
-
-  //on click event for crystals - generates random number for crystal value and logs click to console
-  $(".crystal-img").on("click", function() {
-      var crystalValue = Math.floor(Math.random() * 12) + 1;
-      
-      console.log(crystalValue);
+  
+  //Generate random number for each crystal
+  var amethyst = Math.floor(Math.random() * 12) + 1;
+  var quartz = Math.floor(Math.random() * 12) + 1;
+  var citrine = Math.floor(Math.random() * 12) + 1;
+  var labradorite = Math.floor(Math.random() * 12) + 1;
+  
+   //on click event for each crystal - generates random number for crystal value and logs click to console
+  $("#amethyst").on("click", function() {
+    console.log(amethyst);
   });
+
+  $("#quartz").on("click", function() {
+    console.log(quartz);
+  })
+
+  $("#citrine").on("click", function() {
+    console.log(citrine);
+  })
+
+  $("#labradorite").on("click", function() {
+    console.log(labradorite);
+  })
+
+  console.log(score);
 })
